91e3a6b1dbf14afca88ae673258d0df247fcfb74
conformist-mw <conformist.mw@gmail.com>
2016-10-08 18:37:54 +0300

    done the task

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,20 @@
+import re
+from collections import Counter
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as f:
+        text = f.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    d = Counter(re.findall(r'[\w]+', text))
+    for i in d.most_common(10):
+        print(i[0])
 
 
 if __name__ == '__main__':
-    pass
+    file = load_data(sys.argv[1])
+    get_most_frequent_words(file)
